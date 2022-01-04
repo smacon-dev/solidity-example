50f597e1a49535a30a73d4fa1f7141dba1cfe747
Moto Sakanosita <moto@sakanosita.org>
2022-01-04 22:43:12 +0900

    chore

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,7 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+const ROPSTEN_RPC_URL = process.env.ROPSTEN_RPC_URL;
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `${ROPSTEN_RPC_URL}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
