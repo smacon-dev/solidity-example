c9032a36b3aa050990addfd14822a06f4becaacf
Moto Sakanosita <moto@sakanosita.org>
2022-01-09 14:36:05 +0900

    chore

---
+++ first-app/scripts/deploy.js
@@ -0,0 +1,18 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+  const Factory = await hre.ethers.getContractFactory("Counter");
+  const contract = await Factory.deploy();
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
