abde2581a12f182330ca17fcc64272858ad546ba
Moto Sakanosita <moto@sakanosita.org>
2022-01-16 21:10:21 +0900

    chore

---
+++ merkle-tree/scripts/deploy.js
@@ -0,0 +1,18 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+  const Factory = await hre.ethers.getContractFactory("MerkleProof");
+  const contract = await Factory.deploy();
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
